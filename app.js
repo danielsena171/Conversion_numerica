211bbb4455e541edce72ea18ddd4c2996f5080f5
danielsena171 <dsenacausil71@correo.unicordoba.edu.co>
2020-10-24 13:28:41 -0500

    Update App.js
    Participó Daniel

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const express = require('express');
 const app = express(); 
 const port = 3000; 
 app.get('/', (req, res) => { 
-  res.send('hola, soy daniel, institucional. hola, soy oscar, colaborador. hola soy sebastian, colaborador.'); 
-  res.send(' hola soy sebastian, colaborador.'); 
+  res.send(' hola soy sebastian, colaborador. Hola soy daniel, institucional.'); 
 }) 
 
 app.listen(port, () => { 
