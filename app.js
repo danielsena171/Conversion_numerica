6258feaf683adecfa7779c70160f7ca9f2e82125
sebitas2019 <sbermudezceballos79@correo.unicordoba.edu.co>
2020-10-24 13:26:07 -0500

    Update App.js
    Participacion de los otros 2 integrantes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000; 
 app.get('/', (req, res) => { 
   res.send('hola, soy daniel, institucional. hola, soy oscar, colaborador. hola soy sebastian, colaborador.'); 
-  res.send('hola, soy daniel sena institucional. hola, soy oscar, colaborador. hola soy sebastian, colaborador.'); 
+  res.send(' hola soy sebastian, colaborador.'); 
 }) 
 
 app.listen(port, () => { 
